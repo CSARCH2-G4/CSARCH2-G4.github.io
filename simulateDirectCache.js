0a2b5b6ce91df237319a7a465aaee8115959c1ea
Stephen Salamante <stephen_salamante@dlsu.edu.ph>
2021-01-30 01:31:22 +0800

    Minor frontend revision + added error catcher for main memory values
    Alert when there's no string of integers inputted

--- simulateDirectCache.js
+++ simulateDirectCache.js
@@ -1,47 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
     var viewAs, bloc2kSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime, memoryMap
     // Value Sequence Builder
     var sequence = [];
-    $("#addValues").click(function() {
-        viewAs = $('input[name=flexRadioDefault]:checked', '#viewform').val();
+    $("#addValues").click(function () {
+        if ($("#input_mainMemoryMap").val() == '') {
+            alert('Main Memory Values cannot be empty!')
+        } else {
+            viewAs = $('input[name=flexRadioDefault]:checked', '#viewform').val();
 
-        // Get value fromt textbox
-        var stringSequence = $("#input_mainMemoryMap").val();
-        // Separate each using comma
-        var arraySequence = stringSequence.split(",");
-        // Trim white spaces
-        var noSpace = $.map(arraySequence, $.trim);
-
-
-        var integerSequence = noSpace.map(function(x) {
-            return parseInt(x, 10);
-        });
-
-        // Get multiplier
-        var multiplier = parseInt($("#input_mainMemoryMult").val());
-
-        // Add to sequence
-        for(var i = 0; i < multiplier; i++) {
-            sequence = sequence.concat(integerSequence);
-        }
+            // Get value fromt textbox
+            var stringSequence = $("#input_mainMemoryMap").val();
+            // Separate each using comma
+            var arraySequence = stringSequence.split(",");
+            // Trim white spaces
+            var noSpace = $.map(arraySequence, $.trim);
 
 
-        // Add to table
-        $("#sequenceBody").empty();
-        for(var i = 0; i < sequence.length; i++){
-            var row = "<tr> <td>" + i + "</td>" + "<td> "+ sequence[i] + "</td> </tr>"
-            $("#sequenceBody").append(row);
+            var integerSequence = noSpace.map(function (x) {
+                return parseInt(x, 10);
+            });
+
+            // Get multiplier
+            var multiplier = parseInt($("#input_mainMemoryMult").val());
+
+            // Add to sequence
+            for (var i = 0; i < multiplier; i++) {
+                sequence = sequence.concat(integerSequence);
+            }
+
+
+            // Add to table
+            $("#sequenceBody").empty();
+            for (var i = 0; i < sequence.length; i++) {
+                var row = "<tr> <td>" + i + "</td>" + "<td> " + sequence[i] + "</td> </tr>"
+                $("#sequenceBody").append(row);
+            }
         }
 
         console.log(sequence);
     });
 
-    $("#submitInputs").click(function() {
+    $("#submitInputs").click(function () {
         viewAs = $('input[name=flexRadioDefault]:checked', '#viewform').val();
         viewSizeAs = $('input[name=radioUnit]:checked', '#viewformSize').val();
         block2kSize = parseInt($("#input_blocksize").val());
-        if(viewSizeAs == 'block') {
+        if (viewSizeAs == 'block') {
             mainMemorySize = parseInt($("#input_mmsize").val());
             cacheMemorySize = parseInt($("#input_cmsize").val());
         }
@@ -58,13 +62,13 @@ $(document).ready(function() {
         var validBlock2kSize = powerOfTwo(block2kSize);
         var validmainMemorySize = powerOfTwo(mainMemorySize);
         var validcacheMemorySize = powerOfTwo(cacheMemorySize);
-        var validmemAccessTime  = checkPositive(memAccessTime);
+        var validmemAccessTime = checkPositive(memAccessTime);
         var validcacheAccessTime = checkPositive(cacheAccessTime);
         var validAll = validBlock2kSize && validmainMemorySize && validcacheMemorySize && validmemAccessTime && validcacheAccessTime;
 
         // console.log(validBlock2kSize + " " + validmainMemorySize + " " + validcacheMemorySize + " " + validmemAccessTime + " " + validcacheAccessTime + " ");
         // Block Size Validation
-        if(validBlock2kSize) {
+        if (validBlock2kSize) {
             $("#input_blocksize").removeClass("is-invalid");
             $("#input_blocksize").addClass("is-valid");
         }
@@ -74,7 +78,7 @@ $(document).ready(function() {
         }
 
         // Main Memory Size Validation
-        if(validmainMemorySize) {
+        if (validmainMemorySize) {
             $("#input_mmsize").removeClass("is-invalid");
             $("#input_mmsize").addClass("is-valid");
         }
@@ -83,8 +87,8 @@ $(document).ready(function() {
             $("#input_mmsize").removeClass("is-valid");
         }
 
-         // Cache Memory Size Validation
-         if(validcacheMemorySize) {
+        // Cache Memory Size Validation
+        if (validcacheMemorySize) {
             $("#input_cmsize").removeClass("is-invalid");
             $("#input_cmsize").addClass("is-valid");
         }
@@ -93,8 +97,8 @@ $(document).ready(function() {
             $("#input_cmsize").removeClass("is-valid");
         }
 
-         // Memory Access Time Validation
-         if(validmemAccessTime) {
+        // Memory Access Time Validation
+        if (validmemAccessTime) {
             $("#input_memaccesstime").removeClass("is-invalid");
             $("#input_memaccesstime").addClass("is-valid");
         }
@@ -103,8 +107,8 @@ $(document).ready(function() {
             $("#input_memaccesstime").removeClass("is-valid");
         }
 
-         // Memory Access Time Validation
-         if(validcacheAccessTime) {
+        // Memory Access Time Validation
+        if (validcacheAccessTime) {
             $("#input_cacheaccesstime").removeClass("is-invalid");
             $("#input_cacheaccesstime").addClass("is-valid");
         }
@@ -113,7 +117,7 @@ $(document).ready(function() {
             $("#input_cacheaccesstime").removeClass("is-valid");
         }
 
-        if(validAll) {
+        if (validAll) {
             console.log('hello')
             simulation(viewAs, block2kSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime)
         }
@@ -128,102 +132,102 @@ $(document).ready(function() {
     }
 
     function powerOfTwo(x) {
-        return (Math.log(x)/Math.log(2)) % 1 === 0;
+        return (Math.log(x) / Math.log(2)) % 1 === 0;
     }
 
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
 
-    $("#gooutput").click(function(){
+    $("#gooutput").click(function () {
         $(".input-card").hide();
         $(".output-card").show();
         $(".simulation-card").hide();
         $(".retry-button").show();
         $(".poutput-button").hide();
-    
+
         directMap = simulate(viewAs, block2kSize, mainMemorySize, cacheMemorySize, memoryMap, memAccessTime, cacheAccessTime);
         printVals(directMap)
     });
-    
-    async function simulation(viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime){
+
+    async function simulation(viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime) {
         memoryMap = mainMemoryMap
         directMap = simulate(viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime);
         console.log(directMap)
         snapshots = directMap.cacheSnapshot
         cacheSize = snapshots[0].length
-    
+
         $("#simulationTable").empty()
         $("#inputSimulation").empty()
-    
-        for(var x=0 ; x<cacheSize ; x++) {
-            $("#simulationTable").append("<tr> <th scope=\"row\">"+x+"</th><td id=\"block"+x+"\" class=\"text-right\"></td> </tr>");
-            $("#block"+x).val('E');
-        }
-    
-        for(var x=0 ; x<memoryMap.length ; x++) {
-            $("#inputSimulation").append("<div class=\"text-center col\" id=\"input"+x+"\">"+memoryMap[x]+"</div>");
-        }
-    
-        $("#simulation-body").prop('hidden', false);
-    
-        for(var y=0 ; y<snapshots.length ; y++) {
-            for(var x=0 ; x<cacheSize ; x++) {
-                var text = $("#block"+x).val();
-    
-                if(snapshots[y][x]!=null){
-                    $("#block"+x).text(snapshots[y][x]);
-                    $("#block"+x).val(snapshots[y][x]);
-                }  
-    
-                if(memoryMap[y]==snapshots[y][x])
-                    $("#block"+x).attr('style', 'color:de9918');
-                else
-                    $("#block"+x).attr('style', 'color:black');
 
-                if(snapshots[y][x]==text && memoryMap[y]==text){
+        for (var x = 0; x < cacheSize; x++) {
+            $("#simulationTable").append("<tr> <th scope=\"row\">" + x + "</th><td id=\"block" + x + "\" class=\"text-right\"></td> </tr>");
+            $("#block" + x).val('E');
+        }
+
+        for (var x = 0; x < memoryMap.length; x++) {
+            $("#inputSimulation").append("<div class=\"text-center col\" id=\"input" + x + "\">" + memoryMap[x] + "</div>");
+        }
+
+        $("#simulation-body").prop('hidden', false);
+
+        for (var y = 0; y < snapshots.length; y++) {
+            for (var x = 0; x < cacheSize; x++) {
+                var text = $("#block" + x).val();
+
+                if (snapshots[y][x] != null) {
+                    $("#block" + x).text(snapshots[y][x]);
+                    $("#block" + x).val(snapshots[y][x]);
+                }
+
+                if (memoryMap[y] == snapshots[y][x])
+                    $("#block" + x).attr('style', 'color:de9918');
+                else
+                    $("#block" + x).attr('style', 'color:black');
+
+                if (snapshots[y][x] == text && memoryMap[y] == text) {
                     hits = parseInt($("#hits").text())
                     hits++
-                    $("#block"+x).text("HIT! "+snapshots[y][x]);
+                    $("#block" + x).text("HIT! " + snapshots[y][x]);
                     $("#hits").text(hits)
                 }
-                else if(snapshots[y][x]!=text && memoryMap[y]==snapshots[y][x]){
+                else if (snapshots[y][x] != text && memoryMap[y] == snapshots[y][x]) {
                     misses = parseInt($("#misses").text())
                     misses++
-                    $("#block"+x).text("MISS "+snapshots[y][x]);
+                    $("#block" + x).text("MISS " + snapshots[y][x]);
                     $("#misses").text(misses)
                 }
             }
-            $("#input"+y).attr('style', 'color:ebba34');
+            $("#input" + y).attr('style', 'color:ebba34');
             await sleep(2000);
 
         }
-    
-        for(var x=0 ; x<cacheSize ; x++) {
-            $("#block"+x).attr('style', 'color:black');
-            if(snapshots[memoryMap.length-1][x]!=null)
-                $("#block"+x).text(snapshots[memoryMap.length-1][x]);
+
+        for (var x = 0; x < cacheSize; x++) {
+            $("#block" + x).attr('style', 'color:black');
+            if (snapshots[memoryMap.length - 1][x] != null)
+                $("#block" + x).text(snapshots[memoryMap.length - 1][x]);
             else
-                $("#block"+x).text('E');
+                $("#block" + x).text('E');
         }
     };
-    
-    function printVals(directMap){
+
+    function printVals(directMap) {
         snapshots = directMap.cacheSnapshot
         cacheSize = snapshots[0].length
-    
-        $("#output-body").append('<p class="card-text"><b>Number of Cache Hits:</b>'+directMap.cacheHit+'</p>')
-        $("#output-body").append('<p class="card-text"><b>Number of Cache Miss:</b>'+directMap.cacheMiss+'</p>')
-        $("#output-body").append('<p class="card-text"><b>Miss Penalty (seconds):</b>'+directMap.missPenalty+'</p>')
-        $("#output-body").append('<p class="card-text"><b>Average Memory Access Time (seconds):</b>'+directMap.aveAccessTime+'</p>')
-        $("#output-body").append('<p class="card-text"><b>Total Memory Access Time (seconds):</b>'+directMap.totalAccessTime+'</p>')
+
+        $("#output-body").append('<p class="card-text"><b>Number of Cache Hits:</b>' + directMap.cacheHit + '</p>')
+        $("#output-body").append('<p class="card-text"><b>Number of Cache Miss:</b>' + directMap.cacheMiss + '</p>')
+        $("#output-body").append('<p class="card-text"><b>Miss Penalty (seconds):</b>' + directMap.missPenalty + '</p>')
+        $("#output-body").append('<p class="card-text"><b>Average Memory Access Time (seconds):</b>' + directMap.aveAccessTime + '</p>')
+        $("#output-body").append('<p class="card-text"><b>Total Memory Access Time (seconds):</b>' + directMap.totalAccessTime + '</p>')
         $("#output-body").append('<p class="card-text"><b>Final Snapshot of Cache Memory:</p>')
         $("#output-body").append("<div class=\"row\"><div class=\"col\"><table class=\"table\" style='width: 40%'><thead class=\"thead-dark\"><tr><th scope=\"col\">Block Number</th><th scope=\"col\">Data</th></tr></thead><tbody id='snapshotTable'></tbody></table></div></div>")
-        for(var x=0 ; x<cacheSize ; x++) {
-            if(snapshots[memoryMap.length-1][x]!=null)
-                $("#snapshotTable").append("<tr> <th scope=\"row\">"+x+"</th><td>"+snapshots[memoryMap.length-1][x]+"</td> </tr>");
+        for (var x = 0; x < cacheSize; x++) {
+            if (snapshots[memoryMap.length - 1][x] != null)
+                $("#snapshotTable").append("<tr> <th scope=\"row\">" + x + "</th><td>" + snapshots[memoryMap.length - 1][x] + "</td> </tr>");
             else
-                $("#snapshotTable").append("<tr> <th scope=\"row\">"+x+"</th><td>E</td> </tr>");
+                $("#snapshotTable").append("<tr> <th scope=\"row\">" + x + "</th><td>E</td> </tr>");
         }
     }
 
@@ -239,10 +243,10 @@ function convertToBlock(blockSize, mainMemorySize, cacheMemorySize, mainMemoryMa
     const tag = mainMemorySize - w - k;
 
     // TODO: Do conversion function
-    for(var i = 0; i < mainMemoryMap.length; i++){
+    for (var i = 0; i < mainMemoryMap.length; i++) {
         var stringBinary = mainMemoryMap[i].toString();
-        var removeWord = stringBinary.slice(0, 0-w);
-        var binaryString = removeWord.slice(0-k);
+        var removeWord = stringBinary.slice(0, 0 - w);
+        var binaryString = removeWord.slice(0 - k);
         // console.log("String: " + stringBinary);
         // console.log("remove w: " + removeWord);
         // console.log("binary string : " + binaryString);
@@ -266,7 +270,7 @@ function convertToBlock(blockSize, mainMemorySize, cacheMemorySize, mainMemoryMa
  * @param {Number} memAccessTime Number that represents the time taken to access the main memory. Keep unit the same as cacheAccessTime
  * @param {Number} cacheAccessTime Number that represents the time taken to access the cache memory. Keep unit the same as memAccessTime
  */
-function simulate (viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime) {
+function simulate(viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime) {
 
     let cacheHit = 0,
         cacheMiss = 0,
@@ -287,7 +291,7 @@ function simulate (viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemor
     mainMemoryMap.forEach(blockNum => {
         let blockMap = blockNum % cacheMemorySize;
 
-        if (cache[blockMap] === null || cache[blockMap] !== blockNum ) {
+        if (cache[blockMap] === null || cache[blockMap] !== blockNum) {
             cacheMiss += 1;
         } else {
             cacheHit += 1;
@@ -298,12 +302,12 @@ function simulate (viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemor
     });
     console.log(cacheSnapshot)
 
-    aveAccessTime = ( cacheHit / (cacheHit + cacheMiss) ) * cacheAccessTime +
-                    ( cacheMiss / (cacheHit + cacheMiss) ) * missPenalty;
+    aveAccessTime = (cacheHit / (cacheHit + cacheMiss)) * cacheAccessTime +
+        (cacheMiss / (cacheHit + cacheMiss)) * missPenalty;
 
     totalAccessTime = cacheHit * 2 * cacheAccessTime +
-                        cacheMiss * 2 * (memAccessTime + cacheAccessTime) +
-                        cacheMiss * cacheAccessTime;
+        cacheMiss * 2 * (memAccessTime + cacheAccessTime) +
+        cacheMiss * cacheAccessTime;
     return { cacheHit, cacheMiss, missPenalty, aveAccessTime, totalAccessTime, cacheSnapshot }
 }
 
