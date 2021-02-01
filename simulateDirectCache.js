682683c4bb9ec4a4b64d770740f1317fea2c67e2
Trisha <48178740+trishapelagio@users.noreply.github.com>
2021-02-01 23:06:31 +0800

    Revert "Fixed bug regarding simulation"
    This reverts commit f7387cbb8ae5d8f82aa914a24f3b32c2a9012995.

--- simulateDirectCache.js
+++ simulateDirectCache.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
 
     var viewAs, bloc2kSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime, memoryMap
@@ -10,7 +9,6 @@ $(document).ready(function () {
         } 
         else {
             viewAs = $('input[name=flexRadioDefault]:checked', '#viewform').val();
-        }
 
             // Get value fromt textbox
             var stringSequence = $("#input_mainMemoryMap").val();
@@ -84,12 +82,9 @@ $(document).ready(function () {
     });
 
     $("#submitInputs").click(function () {
-        viewInputAs = $('input[name=flexRadioDefault]:checked', '#viewform').val();
+        viewAs = $('input[name=flexRadioDefault]:checked', '#viewform').val();
         viewSizeAs = $('input[name=radioUnit]:checked', '#viewformSize').val();
         block2kSize = parseInt($("#input_blocksize").val());
-        mainMemorySize = parseInt($("#input_mmsize").val());
-        cacheMemorySize = parseInt($("#input_cmsize").val());
-        /*
         if (viewSizeAs == 'block') {
             mainMemorySize = parseInt($("#input_mmsize").val());
             cacheMemorySize = parseInt($("#input_cmsize").val());
@@ -98,18 +93,15 @@ $(document).ready(function () {
             mainMemorySize = parseInt($("#input_mmsize").val()) / block2kSize;
             cacheMemorySize = parseInt($("#input_cmsize").val()) / block2kSize;
         }
-        */
         mainMemoryMap = sequence;
         memAccessTime = parseFloat($("#input_memaccesstime").val());
         cacheAccessTime = parseFloat($("#input_cacheaccesstime").val());
         console.log(viewSizeAs);
 
         // Validation
-        let validConfig = viewSizeAs === 'word' && validDivisible(block2kSize, mainMemorySize, cacheMemorySize) || viewSizeAs === 'block';
-
-        var validBlock2kSize = validConfig;
-        var validmainMemorySize = validConfig;
-        var validcacheMemorySize = validConfig;
+        var validBlock2kSize = powerOfTwo(block2kSize);
+        var validmainMemorySize = powerOfTwo(mainMemorySize);
+        var validcacheMemorySize = powerOfTwo(cacheMemorySize);
         var validmemAccessTime = checkPositive(memAccessTime);
         var validcacheAccessTime = checkPositive(cacheAccessTime);
         var validAll = validBlock2kSize && validmainMemorySize && validcacheMemorySize && validmemAccessTime && validcacheAccessTime;
@@ -166,7 +158,8 @@ $(document).ready(function () {
         }
 
         if (validAll) {
-            simulation(viewInputAs, viewSizeAs, block2kSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime)
+            console.log('hello')
+            simulation(viewAs, block2kSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime)
             submit();
         }
         else {
@@ -183,11 +176,6 @@ $(document).ready(function () {
         return (Math.log(x) / Math.log(2)) % 1 === 0;
     }
 
-    function validDivisible(blockSize, mainMemorySize, cacheMemorySize) {
-        console.log(cacheMemorySize % blockSize, mainMemorySize % blockSize)
-        return cacheMemorySize % blockSize === 0 && mainMemorySize % blockSize === 0;
-    }
-
     function sleep(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
@@ -199,13 +187,13 @@ $(document).ready(function () {
         $(".retry-button").show();
         $(".poutput-button").hide();
 
-        directMap = simulate(viewInputAs, viewSizeAs, block2kSize, mainMemorySize, cacheMemorySize, memoryMap, memAccessTime, cacheAccessTime);
+        directMap = simulate(viewAs, block2kSize, mainMemorySize, cacheMemorySize, memoryMap, memAccessTime, cacheAccessTime);
         printVals(directMap)
     });
 
-    async function simulation(viewInputAs, viewSizeAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime) {
+    async function simulation(viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime) {
         memoryMap = mainMemoryMap
-        directMap = simulate(viewInputAs, viewSizeAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime);
+        directMap = simulate(viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime);
         console.log(directMap)
         snapshots = directMap.cacheSnapshot
         cacheSize = snapshots[0].length
@@ -285,34 +273,37 @@ $(document).ready(function () {
     }
 
 });
-// console.log(convertToBlock(4, 64, 4, ['111', '110011', '11001001']));
+
+// console.log(convertToBlock( 4, 64, 4, [111, 110011,11001001]));
+
+
 function convertToBlock(blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap) {
     const w = Math.log2(blockSize);
     const k = Math.log2(cacheMemorySize);
 
     const tag = mainMemorySize - w - k;
-    let convertedMap = [];
 
-    /*
+    // TODO: Do conversion function
     for (var i = 0; i < mainMemoryMap.length; i++) {
         var stringBinary = mainMemoryMap[i].toString();
         var removeWord = stringBinary.slice(0, 0 - w);
         var binaryString = removeWord.slice(0 - k);
+        // console.log("String: " + stringBinary);
+        // console.log("remove w: " + removeWord);
+        // console.log("binary string : " + binaryString);
         var integerValue = parseInt(binaryString, 2);
         mainMemoryMap[i] = integerValue;
-    } */
-    mainMemoryMap.forEach(loc => {
-        //console.log(loc, parseInt(loc,2), parseInt(loc,2) >>> w, (parseInt(loc,2) >>> w).toString(2), (parseInt(loc,2) >>> w) % cacheMemorySize, ((parseInt(loc,2) >>> w) % cacheMemorySize).toString(2))
-        convertedMap.push((parseInt(loc,2) >>> w) % cacheMemorySize)
-    });
-
-    return { mainMemorySize, cacheMemorySize, convertedMap };
+    }
+    return { mainMemorySize, cacheMemorySize, mainMemoryMap };
 }
+
+
+
+
 
 /**
  * Simulates the cache mapping function
- * @param {String} viewInputAs Either as 'address' or as 'block'
- * @param {String} viewSizeAs Either as 'block' or as 'word'
+ * @param {String} viewAs Either as 'address' or as 'block'
  * @param {Number} blockSize Integer power of 2 that represents block size in words
  * @param {Number} mainMemorySize Integer power of 2 that represents the size of a memory block in bits
  * @param {Number} cacheMemorySize Integer power of 2 that represents the cache size in words
@@ -320,31 +311,25 @@ function convertToBlock(blockSize, mainMemorySize, cacheMemorySize, mainMemoryMa
  * @param {Number} memAccessTime Number that represents the time taken to access the main memory. Keep unit the same as cacheAccessTime
  * @param {Number} cacheAccessTime Number that represents the time taken to access the cache memory. Keep unit the same as memAccessTime
  */
-function simulate(viewInputAs, viewSizeAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime) {
+function simulate(viewAs, blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap, memAccessTime, cacheAccessTime) {
 
     let cacheHit = 0,
         cacheMiss = 0,
         missPenalty = (cacheAccessTime + memAccessTime) * 2,
         aveAccessTime,
         totalAccessTime = 0,
-        cacheSnapshot = [],
-        mainMemoryBlockMap;
+        cacheSnapshot = [];
 
-    if (viewSizeAs === 'word') {
-        mainMemorySize = mainMemorySize / blockSize;
-        cacheMemorySize = cacheMemorySize / blockSize;
-    }
-
-    if (viewInputAs === 'address') {
+    if (viewAs === 'address') {
         convResult = convertToBlock(blockSize, mainMemorySize, cacheMemorySize, mainMemoryMap);
-        mainMemoryBlockMap = convResult.convertedMap;
-    } else {
-        mainMemoryBlockMap = mainMemoryMap;
+        mainMemorySize = convResult.mainMemorySize;
+        cacheMemorySize = convResult.cacheMemorySize;
+        mainMemoryMap = convResult.mainMemoryMap;
     }
 
     let cache = Array(cacheMemorySize).fill(null);
 
-    mainMemoryBlockMap.forEach((blockNum, i) => {
+    mainMemoryMap.forEach(blockNum => {
         let blockMap = blockNum % cacheMemorySize;
 
         if (cache[blockMap] === null || cache[blockMap] !== blockNum) {
@@ -353,7 +338,7 @@ function simulate(viewInputAs, viewSizeAs, blockSize, mainMemorySize, cacheMemor
             cacheHit += 1;
         }
 
-        cache[blockMap] = mainMemoryMap[i];
+        cache[blockMap] = blockNum;
         cacheSnapshot.push([...cache]);
     });
     console.log(cacheSnapshot)
@@ -367,6 +352,5 @@ function simulate(viewInputAs, viewSizeAs, blockSize, mainMemorySize, cacheMemor
     return { cacheHit, cacheMiss, missPenalty, aveAccessTime, totalAccessTime, cacheSnapshot }
 }
 
-// BUG: Input moves to simulation even when input is invalod
-// TODO: Redo input confirmation to make sure that memorysize is divisible by block size instead
-// FIXME: bug for view as address
+// console.log(simulate('block', 2, 16, 4, [1, 7, 5, 0, 2, 1, 5, 6, 5, 2, 2, 0], 10, 1));
+
